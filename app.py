b38603f00056a6cc7ba65c0418194d7831bd697c
myeunee <myeunee@gmail.com>
2024-10-30 04:17:25 +0900

    GitHub Actions Workflow 파일 설정

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image
+
+# 이미지 불러오기
+image1 = Image.open("patturning_bg.png")
+image2 = Image.open("guideline.png")
 
 st.title('PatTurning Official page')
 st.write('This is PatTurning Official Page.')
